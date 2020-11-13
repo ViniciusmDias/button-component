8e4492ea20150b32dbd8d7ba92827fdb490ff022
Vinicius Dias <vinicius-mdias@hotmail.com>
2020-11-13 15:38:32 -0300

    Style the Hero component

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,98 +1,98 @@
 import React from 'react';
 
-import { HeroContainer, ButtonsContainer } from './styles';
+import { HeroContainer, ButtonsContainer, Buttons } from './styles';
 
 const Hero: React.FC = () => {
   return (
     <HeroContainer>
       <h1>Buttons</h1>
       <ButtonsContainer>
-        <div>
+        <Buttons>
           <strong>{`<Button variant=”outline” />`}</strong>
           <button type="button">Default</button>
-        </div>
-        <div>
+        </Buttons>
+        <Buttons>
           <strong>{`<Button variant=”outline” />`}</strong>
           <button type="button">Default</button>
-        </div>
+        </Buttons>
       </ButtonsContainer>
       <ButtonsContainer>
-        <div>
+        <Buttons>
           <strong>{`<Button variant=”outline” />`}</strong>
           <button type="button">Default</button>
-        </div>
-        <div>
+        </Buttons>
+        <Buttons>
           <strong>{`<Button variant=”outline” />`}</strong>
           <button type="button">Default</button>
-        </div>
+        </Buttons>
       </ButtonsContainer>
       <ButtonsContainer>
-        <div>
+        <Buttons>
           <strong>{`<Button variant=”outline” />`}</strong>
           <button type="button">Default</button>
-        </div>
-        <div>
+        </Buttons>
+        <Buttons>
           <strong>{`<Button variant=”outline” />`}</strong>
           <button type="button">Default</button>
-        </div>
+        </Buttons>
       </ButtonsContainer>
       <ButtonsContainer>
-        <div>
+        <Buttons>
           <strong>{`<Button variant=”outline” />`}</strong>
           <button type="button">Default</button>
-        </div>
+        </Buttons>
       </ButtonsContainer>
       <ButtonsContainer>
-        <div>
+        <Buttons>
           <strong>{`<Button variant=”outline” />`}</strong>
           <button type="button">Default</button>
-        </div>
-        <div>
+        </Buttons>
+        <Buttons>
           <strong>{`<Button variant=”outline” />`}</strong>
           <button type="button">Default</button>
-        </div>
+        </Buttons>
       </ButtonsContainer>
       <ButtonsContainer>
-        <div>
+        <Buttons>
           <strong>{`<Button variant=”outline” />`}</strong>
           <button type="button">Default</button>
-        </div>
-        <div>
+        </Buttons>
+        <Buttons>
           <strong>{`<Button variant=”outline” />`}</strong>
           <button type="button">Default</button>
-        </div>
+        </Buttons>
       </ButtonsContainer>
       <ButtonsContainer>
-        <div>
+        <Buttons>
           <strong>{`<Button variant=”outline” />`}</strong>
           <button type="button">Default</button>
-        </div>
-        <div>
+        </Buttons>
+        <Buttons>
           <strong>{`<Button variant=”outline” />`}</strong>
           <button type="button">Default</button>
-        </div>
-        <div>
+        </Buttons>
+        <Buttons>
           <strong>{`<Button variant=”outline” />`}</strong>
           <button type="button">Default</button>
-        </div>
+        </Buttons>
       </ButtonsContainer>
       <ButtonsContainer>
-        <div>
+        <Buttons>
           <strong>{`<Button variant=”outline” />`}</strong>
           <button type="button">Default</button>
-        </div>
-        <div>
+        </Buttons>
+        <Buttons>
           <strong>{`<Button variant=”outline” />`}</strong>
           <button type="button">Default</button>
-        </div>
-        <div>
+        </Buttons>
+        <Buttons>
           <strong>{`<Button variant=”outline” />`}</strong>
           <button type="button">Default</button>
-        </div>
-        <div>
+        </Buttons>
+        <Buttons>
           <strong>{`<Button variant=”outline” />`}</strong>
           <button type="button">Default</button>
-        </div>
+        </Buttons>
       </ButtonsContainer>
     </HeroContainer>
   );
--- src/components/Hero/styles.ts
+++ src/components/Hero/styles.ts
@@ -1,5 +1,50 @@
 import styled from 'styled-components';
 
-export const HeroContainer = styled.section``;
+export const HeroContainer = styled.section`
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  padding: 4vh 5%;
+  background: var(--e-global-color-background-primary);
 
-export const ButtonsContainer = styled.section``;
+  h1 {
+    color: var(--e-global-color-text-default);
+    margin-bottom: 3vh;
+  }
+`;
+
+export const ButtonsContainer = styled.section`
+  display: flex;
+
+  & + & {
+    margin-top: 5vh;
+  }
+`;
+
+// create a component
+export const Buttons = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  & + & {
+    margin-left: 5%;
+  }
+
+  strong {
+    font-family: 'Ubuntu Mono';
+    font-size: 0.9rem;
+    line-height: 12px;
+    margin-bottom: 1vh;
+    color: var(--e-global-color-button-text-span);
+    font-weight: 400;
+  }
+
+  button {
+    padding: 1vh 2vw;
+    width: max-content;
+    background: #e0e0e0;
+    box-shadow: 0px 2px 3px rgba(51, 51, 51, 0.2);
+    border-radius: 6px;
+    border: none;
+  }
+`;

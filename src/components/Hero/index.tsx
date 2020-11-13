2269b8267023cad68e710a49418b6cfc96e71d1e
Vinicius Dias <vinicius-mdias@hotmail.com>
2020-11-13 15:00:16 -0300

    Create the structure

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,9 +1,101 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { HeroContainer, ButtonsContainer } from './styles';
 
 const Hero: React.FC = () => {
-  return <div />;
+  return (
+    <HeroContainer>
+      <h1>Buttons</h1>
+      <ButtonsContainer>
+        <div>
+          <strong>{`<Button variant=”outline” />`}</strong>
+          <button type="button">Default</button>
+        </div>
+        <div>
+          <strong>{`<Button variant=”outline” />`}</strong>
+          <button type="button">Default</button>
+        </div>
+      </ButtonsContainer>
+      <ButtonsContainer>
+        <div>
+          <strong>{`<Button variant=”outline” />`}</strong>
+          <button type="button">Default</button>
+        </div>
+        <div>
+          <strong>{`<Button variant=”outline” />`}</strong>
+          <button type="button">Default</button>
+        </div>
+      </ButtonsContainer>
+      <ButtonsContainer>
+        <div>
+          <strong>{`<Button variant=”outline” />`}</strong>
+          <button type="button">Default</button>
+        </div>
+        <div>
+          <strong>{`<Button variant=”outline” />`}</strong>
+          <button type="button">Default</button>
+        </div>
+      </ButtonsContainer>
+      <ButtonsContainer>
+        <div>
+          <strong>{`<Button variant=”outline” />`}</strong>
+          <button type="button">Default</button>
+        </div>
+      </ButtonsContainer>
+      <ButtonsContainer>
+        <div>
+          <strong>{`<Button variant=”outline” />`}</strong>
+          <button type="button">Default</button>
+        </div>
+        <div>
+          <strong>{`<Button variant=”outline” />`}</strong>
+          <button type="button">Default</button>
+        </div>
+      </ButtonsContainer>
+      <ButtonsContainer>
+        <div>
+          <strong>{`<Button variant=”outline” />`}</strong>
+          <button type="button">Default</button>
+        </div>
+        <div>
+          <strong>{`<Button variant=”outline” />`}</strong>
+          <button type="button">Default</button>
+        </div>
+      </ButtonsContainer>
+      <ButtonsContainer>
+        <div>
+          <strong>{`<Button variant=”outline” />`}</strong>
+          <button type="button">Default</button>
+        </div>
+        <div>
+          <strong>{`<Button variant=”outline” />`}</strong>
+          <button type="button">Default</button>
+        </div>
+        <div>
+          <strong>{`<Button variant=”outline” />`}</strong>
+          <button type="button">Default</button>
+        </div>
+      </ButtonsContainer>
+      <ButtonsContainer>
+        <div>
+          <strong>{`<Button variant=”outline” />`}</strong>
+          <button type="button">Default</button>
+        </div>
+        <div>
+          <strong>{`<Button variant=”outline” />`}</strong>
+          <button type="button">Default</button>
+        </div>
+        <div>
+          <strong>{`<Button variant=”outline” />`}</strong>
+          <button type="button">Default</button>
+        </div>
+        <div>
+          <strong>{`<Button variant=”outline” />`}</strong>
+          <button type="button">Default</button>
+        </div>
+      </ButtonsContainer>
+    </HeroContainer>
+  );
 };
 
 export default Hero;
---
+++ src/components/Hero/styles.ts
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+export const HeroContainer = styled.section``;
+
+export const ButtonsContainer = styled.section``;
